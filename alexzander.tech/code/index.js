5e98caf365835dcee02685eb2f911b1dfdfdcb0d
alexzanderr <54772464+alexzanderr@users.noreply.github.com>
2021-06-26 11:28:52 +0300

    Delete alexzander.tech directory

--- alexzander.tech/code/index.js
+++
@@ -1,8 +0,0 @@
-
-// function reportWindowSize() {
-//   console.log(window.innerHeight)
-//   console.log(window.innerWidth)
-// }
-//
-// window.onresize = reportWindowSize;
-// // window.addEventListener('resize', reportWindowSize);
